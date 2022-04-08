2d16399b7bfef139e7ed35cf5747b1b44ab9a2cc
busrauz78 <busrauz78@gmail.com>
2022-04-08 04:52:46 +0300

    create get and post service

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.static('assets'));
+const messages = [];
+
+app.use(express.static('client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/messages', (req, res) => {
+  res.status(200).send(messages);
+});
+
+app.post('/messages', (req,res) => {
+  messages.push(req.body);
+  res.sendStatus(200);
+});
+
+app.get('*', (req,res) => {
+  res.status(404).send('Page is not found!');
+});
 
 app.listen(3600, () => {
     console.log('Server is started..');
