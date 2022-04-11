81f5b0070a94b6a28a11ec9ef9347e3517a133b4
Büşra Uz <busrauz78@gmail.com>
2022-04-11 20:06:29 +0300

    Delete dbConfig.js

--- dbConfig.js
+++
@@ -1,7 +0,0 @@
-const dbConfig = {
-    user: 'user',
-    password: 'userqweasdzxc123',
-    url: 'mongodb+srv://user:userqweasdzxc123@cluster0.bvfoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-};
-
-module.exports = dbConfig;
