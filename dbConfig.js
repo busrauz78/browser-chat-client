a35e95e056fb708b0d96d72bc62be57edb9ac4f1
busrauz78 <busrauz78@gmail.com>
2022-04-11 20:01:42 +0300

    added db connection

---
+++ dbConfig.js
@@ -0,0 +1,7 @@
+const dbConfig = {
+    user: 'user',
+    password: 'userqweasdzxc123',
+    url: 'mongodb+srv://user:userqweasdzxc123@cluster0.bvfoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+};
+
+module.exports = dbConfig;
--- index.js
+++ index.js
@@ -1,33 +1,62 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const { url } = require('./dbConfig');
+const mongoose = require('mongoose');
+const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const dbUrl = url;
 
-const messages = [];
+// Model
+const Message = mongoose.model('Message', {
+  name: String,
+  message: String,
+});
 
+// Export client with express
 app.use(express.static('client'));
+
+// Parser for request body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Requests
 app.get('/messages', (req, res) => {
-  res.status(200).send(messages);
+  Message.find({}, (err, messages) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(messages);
+    }
+  })
 });
-
 app.post('/messages', (req,res) => {
-  messages.push(req.body);
-  io.emit('message', req.body);
-  res.sendStatus(200);
+  const message = new Message(req.body);
+  message.save((err) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      io.emit('message', req.body);
+      res.sendStatus(200);
+    }
+  })
+  
 });
-
 app.get('*', (req,res) => {
   res.status(404).send('Page is not found!');
 });
 
+// Socket connection
 io.on('connection', (socket) => {
   console.log('User is connected..');
 });
 
+// Db connection
+mongoose.connect(dbUrl ,(err) => {
+  console.log('Db connection is successfull', err);
+});
+
+// Server connection
 http.listen(3600, () => {
     console.log('Server is started..');
 })
